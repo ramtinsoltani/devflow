92c8874979cfaf2a54c2904eba034298bd3314b3
Ramtin Soltani <ramtinsoltani93@gmail.com>
2025-03-13 04:20:20 -0700

    updated test endpoints

--- src/routers/test.ts
+++ src/routers/test.ts
@@ -1,29 +1,29 @@
 import { Request, Response, Router } from "express";
 import { asyncHandler } from "../lib/middleware/async-handler";
+import { getPreviewFromContent } from "link-preview-js";
 
 export const TestRouter = Router();
 
 TestRouter.post('/proxy', asyncHandler(async (req: Request, res: Response) => {
 
-  fetch(req.body.url, {
+  const response = await fetch(req.body.url, {
     method: 'GET',
     headers: {
       'user-agent': 'google-bot',
       origin: new URL(req.body.url).origin
     },
     redirect: 'follow'
-  })
-  .then(response => {
-
-    return response.text();
-
-  })
-  .then(text => res.send(text))
-  .catch(error => {
-
-    res.json(error);
-
   });
+
+  const preview = await getPreviewFromContent({
+    data: await response.text(),
+    headers: {
+      'content-type': response.headers.get('content-type') || 'text/html; charset=utf-8'
+    },
+    url: req.body.url
+  });
+
+  res.json(preview);
 
 }));
 
@@ -54,10 +54,76 @@ TestRouter.post('/proxy2', asyncHandler(async (req: Request, res: Response) => {
     res.send('REJECTED');
   else {
 
-    const text1 = await responses[0].value.text();
-    const text2 = await responses[1].value.text();
+    const preview1 = await getPreviewFromContent({
+      data: await responses[0].value.text(),
+      headers: {
+        'content-type': responses[0].value.headers.get('content-type') || 'text/html; charset=utf-8'
+      },
+      url: req.body.url
+    });
 
-    res.send(text1 + text2);
+    const preview2 = await getPreviewFromContent({
+      data: await responses[1].value.text(),
+      headers: {
+        'content-type': responses[1].value.headers.get('content-type') || 'text/html; charset=utf-8'
+      },
+      url: req.body.url
+    });
+
+    res.json([
+      preview1,
+      preview2
+    ]);
+
+  }
+
+}));
+
+TestRouter.post('/proxy3', asyncHandler(async (req: Request, res: Response) => {
+
+  const responses = await Promise.allSettled([
+    fetch(req.body.url, {
+      method: 'GET',
+      headers: {
+        'user-agent': 'google-bot',
+        origin: new URL(req.body.url).origin
+      },
+      redirect: 'follow'
+    }),
+    fetch(new URL(req.body.url).origin, {
+      method: 'GET',
+      headers: {
+        'user-agent': 'google-bot',
+        origin: new URL(req.body.url).origin
+      },
+      redirect: 'follow'
+    })
+  ]);
+
+  if ( responses[0].status === 'rejected' )
+    res.send('REJECTED');
+  else if ( responses[1].status === 'rejected' )
+    res.send('REJECTED');
+  else {
+
+    const previews = await Promise.all([
+      getPreviewFromContent({
+        data: await responses[0].value.text(),
+        headers: {
+          'content-type': responses[0].value.headers.get('content-type') || 'text/html; charset=utf-8'
+        },
+        url: req.body.url
+      }),
+      getPreviewFromContent({
+        data: await responses[1].value.text(),
+        headers: {
+          'content-type': responses[1].value.headers.get('content-type') || 'text/html; charset=utf-8'
+        },
+        url: req.body.url
+      })
+    ]);
+
+    res.json(previews);
 
   }
 
